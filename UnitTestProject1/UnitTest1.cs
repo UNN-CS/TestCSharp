bda43f25c5e080f2234cebcb166863205ecdfe65
anton <ashtanyuk@gmail.com>
2024-02-07 16:03:37 +0300

    Начальный коммит

---
+++ UnitTestProject1/UnitTest1.cs
@@ -0,0 +1,31 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.IO;
+
+namespace UnitTestProject1
+{
+    [TestClass]
+    public class UnitTest1
+    {
+
+        [TestMethod()]
+        [DeploymentItem("testFile1.xml")]
+        public void ConstructorTest()
+        {
+            string file = "testFile1.xml";
+            Assert.IsTrue(File.Exists(file), "deployment failed: " + file +
+                " did not get deployed");
+        }
+        [TestMethod]
+        public void TestMethod1()
+        {
+            int result = ConsoleApp1.Program.mul(3, 4);
+            Assert.IsTrue(result == 12);
+        }
+        [TestMethod]
+        public void TestMethod2()
+        {
+            int result = ConsoleApp1.Program.add(3, 4);
+            Assert.IsTrue(result == 7);
+        }
+    }
+}
